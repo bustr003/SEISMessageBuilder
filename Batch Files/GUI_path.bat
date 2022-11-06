2cdb1a56328cf7dcf77ab7d7fb729a8570f3f6c4
Mhea <bustr003@cougars.csusm.edu>
2022-11-05 23:59:10 -0700

    Comment the batch file.

--- Batch Files/GUI_path.bat
+++ Batch Files/GUI_path.bat
@@ -1,8 +1,14 @@
+:: Do not display each line of the batch file.
 @ECHO OFF
+
 :: GUI_path.bat file to go to the correct directory in the command prompt
 :: Useful for being in the correct directory to do git commands.
 
+:: Display the GitHub repository URL.
 echo https://github.com/bustr003/SEISMessageBuilder.git
 
+:: Navigate to the directory
 cd "C:\Users\Mhea Bustria\Desktop\SEISMessageBuilder"
+
+:: Keep the command prompt open to accept more commands.
 cmd
