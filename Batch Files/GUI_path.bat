25d3c1fa030a32d08d4e6ca204e41e9f54f8c3e6
Mhea <bustr003@cougars.csusm.edu>
2022-11-05 23:53:12 -0700

    Add GitHub repo URL to ReadMe and batch file for reference.

--- Batch Files/GUI_path.bat
+++ Batch Files/GUI_path.bat
@@ -1,2 +1,7 @@
+:: GUI_path.bat file to go to the correct directory in the command prompt
+:: Useful for being in the correct directory to do git commands.
+
+:: Link to repo: https://github.com/bustr003/SEISMessageBuilder.git
+
 cd "C:\Users\Mhea Bustria\Desktop\SEISMessageBuilder"
 cmd
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # SEISMessageBuilder
+https://github.com/bustr003/SEISMessageBuilder.git
+
 Message builder related to managing the Special Education Information System
 
 Uses Tkinter to make a GUI.
