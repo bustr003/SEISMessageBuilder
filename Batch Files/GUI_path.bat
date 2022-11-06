e304b9fa80ce3b6c879725a2593dfdd78cb66d00
Mhea <bustr003@cougars.csusm.edu>
2022-11-05 23:57:10 -0700

    Use echo to display the GitHub URL when running the batch file.

--- Batch Files/GUI_path.bat
+++ Batch Files/GUI_path.bat
@@ -1,7 +1,8 @@
+@ECHO OFF
 :: GUI_path.bat file to go to the correct directory in the command prompt
 :: Useful for being in the correct directory to do git commands.
 
-:: Link to repo: https://github.com/bustr003/SEISMessageBuilder.git
+echo https://github.com/bustr003/SEISMessageBuilder.git
 
 cd "C:\Users\Mhea Bustria\Desktop\SEISMessageBuilder"
 cmd
