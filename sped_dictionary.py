0cf24559dbec2ad8e3131cc6a56ceb203faa983e
Mhea <bustr003@cougars.csusm.edu>
2022-11-06 20:59:18 -0800

    Move term definitions to sped_dictionary.py. Move term dictoinary in glossary.py above the function.

---
+++ sped_dictionary.py
@@ -0,0 +1,130 @@
+# FILE sped_dictionary.py
+
+# Definitions
+calpads_def = "California Longitudinal Pupil Achievement Data System"
+
+csat_def = "Comprehensive Student Assistance Teams\n- A general education process for identifying and supporting students."
+
+dhh_def = "Deaf and Hard of Hearing"
+dhh_def += "\n- Program Specialist: Meghan Carlon"
+dhh_def += "\n- AUDITORY ORAL PROGRAM: Students with hearing loss/Cochlear Implant process. Intensive auditory training and instruction to increase listening and speaking skills. Specialized DHH teacher & Speech Therapist."
+dhh_def += "\n- TOTAL COMMUNICATION/SIGNING PROGRAM: Paraeducators who use sign language & certificated ASL interpreters."
+dhh_def += "\n - ITINERANT PROGRAM: Full Time itinerant DHH teacher and audiologist."
+
+dis_def = "Designated Instruction and Services\n- No SAI services, but also not Speech Only.\n- Examples: APE, DHH, OT"
+
+discrete_trial_def = "- Educational strategy based on the principles of applied behavior analysis"
+discrete_trial_def += "\n- Break skills down into smaller components"
+discrete_trial_def += "\n- Teach  sub-skills individually"
+discrete_trial_def += "\n- Repeated practice of skills"
+discrete_trial_def += "\n- Prompting procedures"
+discrete_trial_def += "\n- Reinforcement procedures"
+
+domains_def = "M/S classes teach across the domains."
+domains_def += "\n- ACADEMICS: Whole/small group/individualized instruction for core content and IEP Goals"
+domains_def += "\n- COMMUNICATION: Language development and communication skills with all modes of communication and Augmentative or Alternative Communication in a language-rich environment"
+domains_def += "\n- COMMUNITY SKILLS: Safety skills, transitioning and navigating camps"
+domains_def += "\n- DAILY LIVING SKILLS: Hygiene, feeding, toileting"
+domains_def += "\n- RECREATION/LEISURE: Making choices, individual choice Activities, collaborative games/activities"
+domains_def += "\n- SOCIAL EMOTIONAL: Self regulation, expected behaviors, social interactions"
+domains_def += "\n- VOCATIONAL SKILLS: Classroom jobs, task lists, student store operation, following directions"
+
+dsea_def = "District of SpEd Accountability"
+
+epiphany_def = "Epiphany Prep Charter School"
+epiphany_def += "\nhttps://www.cde.ca.gov/schooldirectory/details?cdscode=37680980133991"
+epiphany_def += "\n- closed"
+
+heritage_def = "American Heritage Charter Schools family of charter schools"
+heritage_def += "\n- Heritage K-8 Charter"
+heritage_def += "\n- SpEd support services offered through EUSD"
+heritage_def += "\n- DSEA is EUSD"
+
+hfa_def = "Heritage Flex Academy"
+hfa_def += "\nhttps://hfa.amhcs.org/about/welcome/"
+hfa_def += "\n- TK-8"
+
+hhi_def = "Home Hospital Instruction\n- A service for students who are unable to attend school in person due to health needs."
+hhi_def += "\n- The student will still have a \"School of Attendance\" that they would normally attend should they be able to attend school physically."
+hhi_def += "\n- Until then, the student receives their services and education in home setting."
+
+lcap_def = "Local Control and Accountability Plan"
+lcap_def += "\n- A critical part of California's new Local Control Funding Formula (LCFF)"
+lcap_def += "\n- A three-year, district-level plan that is updated annually"
+
+lcff_def = "Local Control Funding Formula"
+
+lea_def = "Local Education Agency"
+lea_def += "- EUSD"
+
+lla_def = "Limitless Learning Academy"
+lla_def += "\nhttps://www.eusd.org/o/lla/page/about-us"
+lla_def += "\n- Blended Learning K-8 Option"
+lla_def += "\n- virtual and hybrid learning"
+lla_def += "\n- engaging lessons via Zoom in the morning"
+lla_def += "\n- personalized learning in the afternoon via in-person or virtually"
+
+mm_def = "Mild/Moderate"
+mm_def += "\n- M/M SAI"
+mm_def += "\n- M/M SAI SC"
+
+ms_def = "Moderate-Severe"
+ms_def += "\n- Program Specialist: Alice Abalos"
+ms_def += "\n- M/S classrooms have a SAI teacher and paraeducators who work together to actively engage students in the learning process." 
+ms_def += "\n- Classroom staff work collaboratively with related service providers for a comprehensive approach to addressing each student’s unique needs."
+ms_def += "\n- M/S classes teach across the domains of instruction."
+
+news2you_def = "- Weekly current events stories and related learning activities"
+
+orton_gillingham_def = "Orton-Gillingham (OG) structured literacy approach"
+orton_gillingham_def += "\n- Provides instruction on phonemic awareness, phonics, spelling, grammar"
+
+preschool_def = "https://www.eusd.org/o/preschool/page/eusd-state-preschool-about-us"
+preschool_def += "- Program Specialist: Natalie Muldoon"
+preschool_def += "\n- STATE PRESCHOOL SAI INCLUSION: Provides access to the GenEd environment"
+preschool_def += "\n-- Subsidized programs for low income families"
+preschool_def += "\n-- CSPP: California State Preschool Program"
+preschool_def += "\n-- SDQPI: San Diego Quality Preschool Initiative"
+preschool_def += "\n- NON-CATEGORICAL CLASSES: Peer buddies & paraeducators"
+preschool_def += "\n- SCSB PRESCHOOL CLASSES"
+
+proACT_def = "Pro-ACT"
+proACT_def += "\n- Professional Assault Crisis Training"
+
+program_supports_def = "- A critical component in all moderate to severe programs"
+program_supports_def += "\n- Embedded supports that enhance a child's education"
+program_supports_def += "\n- Examples: communication device, chewy necklace, compression vest"
+
+rsp_sai_def = "\n- Resource Specialist Program\n- Specialized Academic Instruction"
+rsp_sai_def += "\n- Program Specialists, Elementary: Meghan Carlon and Carol Fekete"
+rsp_sai_def += "\n- Program Specialist, Middle: Dena Moore"
+rsp_sai_def += "\n- The M/M SAI program teaches grade level common core standards in conjunction with the GenEd teacher either in the GenEd setting or in the SpEd classroom"
+rsp_sai_def += " so that students feel supported, engaged and committed to meeting their individualized goals."
+
+sc_sdc_def = "\nSeparate Class / Separate Day Class"
+sc_sdc_def += "\n- Program Specialist: Giuliana Forte (Elementary)"
+sc_sdc_def += "\n- The M/M separate class program offers classes that address the unique needs of our students with mild to moderate challenges."
+
+scsb_def = "Structured Communication Social Behavioral classes"
+scsb_def += "\n- Program Specialist: Tracy Lane"
+scsb_def += "\n- Provide some of our most intensive supports to students with Autism or other developmental disabilities"
+scsb_def += "\n- Provide students with intensive supports, predictable, consistent routines and structure throughout the school day to engage the students in learning activities such as functional communication, social skills, academics, and behavior management."
+scsb_def += "\n- Clearly-defined spaces for whole group activities, small group instruction, Discrete Trial Training, independent work, and cool down/break areas"
+
+seas_def = "Social Emotional Academic Support classes"
+seas_def += "\n- previously: Intensive Behavioral Intervention program"
+seas_def += "\n- Program Specialist: Dena Moore"
+seas_def += "\n- Addresses the needs of students exhibiting significant social, emotional, and behavioral concerns"
+seas_def += "\n- Therapeutic/mental health supports"
+seas_def += "\n- CLINICAL SOCIAL WORKERS: individual counseling, groups, risk assessments, parent trainings and consultations, help case manage with outside providers"
+seas_def += "\n- SCHOOL PSYCHOLOGIST: behavioral programming, risk assessments, educationally related mental health assessments"
+seas_def += "\n- Treatment Team: weekly meetings"
+
+selpa_def = "SpEd Local Planning Area"
+selpa_def += "\n- EUSD is in the North Inland SELPA"
+selpa_def += "\n- Heritage K-8 Charter is in the North Inland SELPA"
+
+uls_def = "Unique Learning Systems"
+uls_def += "\n- A standard-aligned curriculum, differentiated materials across all content areas"
+
+# EOF sped_dictionary.py
