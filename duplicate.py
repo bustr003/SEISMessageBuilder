72278bf7b6baaf810729421e6a5a65d8a718c543
Mhealyssah Bustria <mhea.bmail@gmail.com>
2022-11-10 13:20:24 -0800

    Process for de-activating a duplicate record

---
+++ duplicate.py
@@ -0,0 +1,27 @@
+# FILE duplicate.py
+"""
+Process for de-activating a duplicate record
+"""
+
+"""
+ACTIONS:
+This record was created by mistake.
+It must be deleted/de-activated.
+
+The correct record for this student is
+Name: 
+SEIS ID: 
+Record Added on 
+
+Downloaded the Future IEP forms and attachment by Melissa Gerke.
+Sent the documents to Melissa Gerke and advised her to work on the correct record (not this one).
+
+PROCESS FOR DE-ACTIVATING A DUPLICATE RECORD
+Made DNR
+Marked Duplicate Record
+Changed status to DNQ/Not Providing Services
+This record will not be pulled into search results.
+This record will not be pulled into the Add Student search.
+"""
+
+# EOF duplicate.py
